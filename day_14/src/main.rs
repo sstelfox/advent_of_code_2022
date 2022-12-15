9618101d0e08e1995c9f072d745e42a1003a7df2
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2022-12-14 22:38:58 -0500

    alright orient the display correctly, should add tests but think the simulation has been properly parsed

--- day_14/src/main.rs
+++ day_14/src/main.rs
@@ -11,42 +11,81 @@ struct SimulatedEnvironment {
     active_sand: Option<Point>,
     spawner_location: Point,
 
-    tiles: [Tile; SIMULATION_WIDTH * SIMULATION_HEIGHT],
+    tiles: Vec<Tile>,
 }
 
 impl SimulatedEnvironment {
     fn display_string(&self) -> String {
-        let mut chars = vec![];
+        let mut output: Vec<String> = vec![];
 
-        for x in std::ops::RangeInclusive::new(self.aabb.0.x, self.aabb.1.x) {
-            for y in std::ops::RangeInclusive::new(self.aabb.0.y, self.aabb.1.y) {
-                chars.push(self.tiles[x as usize + y as usize * SIMULATION_WIDTH].char());
+        for y in std::ops::RangeInclusive::new(self.aabb.0.y, self.aabb.1.y) {
+            let mut line_chars = vec![];
+
+            for x in std::ops::RangeInclusive::new(self.aabb.0.x, self.aabb.1.x) {
+                line_chars.push(self.get_tile(x, y).char());
             }
 
-            chars.push('\n');
+            output.push(line_chars.into_iter().collect());
         }
 
-        chars.into_iter().collect()
+        output.join("\n")
     }
 
     fn draw_line_segment(&mut self, line: LineSegment) {
         if line.horizontal() {
-            unimplemented!()
+            let (min_x, max_x) = (
+                line.left.x.min(line.right.x),
+                line.left.x.max(line.right.x)
+            );
+
+            self.aabb.0.x = min_x.min(self.aabb.0.x);
+            self.aabb.1.x = max_x.max(self.aabb.1.x);
+
+            for x in std::ops::RangeInclusive::new(min_x, max_x) {
+                self.set_tile(x, line.left.y, Tile::Rock);
+            }
         } else if line.vertical() {
-            unimplemented!()
+            let (min_y, max_y) = (
+                line.left.y.min(line.right.y),
+                line.left.y.max(line.right.y)
+            );
+
+            self.aabb.0.y = min_y.min(self.aabb.0.y);
+            self.aabb.1.y = max_y.max(self.aabb.1.y);
+
+            for y in std::ops::RangeInclusive::new(min_y, max_y) {
+                self.set_tile(line.left.x, y, Tile::Rock);
+            }
         } else {
             panic!("does not support diagonal lines: {:?}", line);
         }
     }
 
+    fn get_tile(&self, x: isize, y: isize) -> Tile {
+        self.tiles[x as usize + y as usize * SIMULATION_WIDTH]
+    }
+
     fn new(spawner_location: Point) -> Self {
-        SimulatedEnvironment {
+        let mut sim_env = SimulatedEnvironment {
             aabb: (spawner_location, spawner_location),
             active_sand: None,
             spawner_location,
 
-            tiles: [Tile::default(); SIMULATION_WIDTH * SIMULATION_HEIGHT],
+            tiles: vec![Tile::default(); SIMULATION_WIDTH * SIMULATION_HEIGHT],
+        };
+
+        sim_env.set_tile(spawner_location.x, spawner_location.y, Tile::Spawner);
+
+        sim_env
+    }
+
+    fn set_tile(&mut self, x: isize, y: isize, tile: Tile) {
+        if self.spawner_location.x == x && self.spawner_location.y == y && tile != Tile::Spawner{
+            println!("attempted overwriting of the spawner location...");
+            return;
         }
+
+        self.tiles[x as usize + y as usize * SIMULATION_WIDTH] = tile;
     }
 }
 
@@ -131,17 +170,23 @@ fn parse_line(data: &str) -> Vec<LineSegment> {
 }
 
 fn parse_simulated_environment(data: &[u8]) -> SimulatedEnvironment {
+    let spawner_location = Point::new(SPAWNER_X, SPAWNER_Y);
+    let mut simulated_environment = SimulatedEnvironment::new(spawner_location);
+
     let data = std::str::from_utf8(data).unwrap();
     let lines_to_draw: Vec<LineSegment> = data.lines().map(|l| parse_line(l)).flatten().collect();
 
-    let spawner_location = Point::new(SPAWNER_X, SPAWNER_Y);
-    let mut simulated_environment = SimulatedEnvironment::new(spawner_location);
+    lines_to_draw.into_iter()
+        .for_each(|l| simulated_environment.draw_line_segment(l));
 
     simulated_environment
 }
 
 fn main() {
-    let _sim_env = parse_simulated_environment(INPUT_DATA);
+    let sim_env = parse_simulated_environment(INPUT_DATA);
+
+    println!("AABB: (({}, {}), ({}, {}))", sim_env.aabb.0.x, sim_env.aabb.0.y, sim_env.aabb.1.x, sim_env.aabb.1.y);
+    println!("{}", sim_env.display_string());
 }
 
 #[cfg(test)]
@@ -172,20 +217,5 @@ mod tests {
     #[test]
     fn test_simulation_parsing() {
         let sim_env = parse_simulated_environment(SAMPLE_INPUT);
-        assert_eq!(sim_env.aabb, (Point::new(494, 0), Point::new(503, 9)));
-
-        println!("boop");
-
-        let expected_display = "..........\n\
-                                ..........\n\
-                                ..........\n\
-                                ..........\n\
-                                ....#...##\n\
-                                ....#...#.\n\
-                                ..###...#.\n\
-                                ........#.\n\
-                                ........#.\n\
-                                #########.";
-        //assert_eq!(sim_env.display_string(), expected_display);
     }
 }
