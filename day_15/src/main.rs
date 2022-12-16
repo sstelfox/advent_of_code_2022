9088bec15d37d891b74f93c55a2cd9b41ac0f688
Sam Stelfox <sstelfox@bedroomprogrammers.net>
2022-12-16 12:04:22 -0500

    well... this isn't going to work hahah

--- day_15/src/main.rs
+++ day_15/src/main.rs
@@ -9,11 +9,60 @@ lazy_static! {
 
 #[derive(Debug)]
 struct Environment {
-    size: (isize, isize),
+    position_offsets: (isize, isize),
+    size: (usize, usize),
+
     tiles: Vec<Tile>,
 }
 
-#[derive(Debug, Eq, PartialEq)]
+impl Environment {
+    fn add_location(&mut self, sensor: &Point, beacon: &Point) {
+        let sensor = Point::new(sensor.x - self.position_offsets.0, sensor.y - self.position_offsets.1);
+        self.set_tile(sensor.x as usize, sensor.y as usize, Tile::Sensor);
+
+        let beacon = Point::new(beacon.x - self.position_offsets.0, beacon.y - self.position_offsets.1);
+        self.set_tile(beacon.x as usize, beacon.y as usize, Tile::Beacon);
+
+        let _manhattan_distance = (sensor.x - beacon.x).abs() + (sensor.y - beacon.y).abs();
+
+        self.tiles[0] = Tile::Unknown;
+    }
+
+    fn display_string(&self) -> String {
+        let mut output: Vec<String> = vec![];
+
+        for y in std::ops::RangeInclusive::new(0, self.size.1) {
+            let mut line_chars = vec![];
+
+            for x in std::ops::RangeInclusive::new(0, self.size.0) {
+                line_chars.push(self.get_tile(x, y).char());
+            }
+
+            output.push(line_chars.into_iter().collect());
+        }
+
+        output.join("\n")
+    }
+
+    fn get_tile(&self, x: usize, y: usize) -> Tile {
+        self.tiles[x + y * self.size.1]
+    }
+
+    fn new(x_size: usize, y_size: usize, x_offset: isize, y_offset: isize) -> Environment {
+        Self {
+            position_offsets: (x_offset, y_offset),
+            size: (x_size, y_size),
+
+            tiles: vec![Tile::default(); x_size * y_size],
+        }
+    }
+
+    fn set_tile(&mut self, x: usize, y: usize, tile: Tile) {
+        self.tiles[x + y * self.size.1] = tile;
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
 struct Point {
     x: isize,
     y: isize,
@@ -25,7 +74,7 @@ impl Point {
     }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 enum Tile {
     Unknown,
     Beacon,
@@ -33,10 +82,58 @@ enum Tile {
     Sensor,
 }
 
+impl Tile {
+    fn char(&self) -> char {
+        use Tile::*;
+
+        match self {
+            Unknown => '.',
+            Beacon => 'B',
+            NoBeacon => '#',
+            Sensor => 'S',
+        }
+    }
+}
+
 impl Default for Tile {
     fn default() -> Self {
         Tile::Unknown
     }
+}
+
+fn parse_environment(data: &[u8]) -> Environment {
+    let data = std::str::from_utf8(data).unwrap();
+    let locations: Vec<(Point, Point)> = data.lines().map(|l| parse_line(l)).collect();
+
+    let mut bounds: Option<(Point, Point)> = None;
+
+    for location in locations.iter().map(|loc| [&loc.0, &loc.1]).flatten() {
+        if let Some((ref mut min, ref mut max)) = bounds {
+            min.x = min.x.min(location.x);
+            max.x = max.x.max(location.x);
+
+            min.y = min.y.min(location.y);
+            max.y = max.y.max(location.y);
+        } else {
+            bounds = Some((location.clone(), location.clone()));
+        }
+    }
+
+    let bounds = bounds.unwrap();
+
+    let size_x = (bounds.1.x - bounds.0.x) as usize;
+    let size_y = (bounds.1.y - bounds.0.y) as usize;
+
+    let offset_x = bounds.0.x;
+    let offset_y = bounds.0.y;
+
+    let mut env = Environment::new(size_x, size_y, offset_x, offset_y);
+
+    for (sensor, beacon) in locations.iter() {
+        env.add_location(sensor, beacon);
+    }
+
+    env
 }
 
 fn parse_line(line: &str) -> (Point, Point) {
@@ -49,6 +146,9 @@ fn parse_line(line: &str) -> (Point, Point) {
 }
 
 fn main() {
+    let environment = parse_environment(INPUT_DATA);
+
+    println!("{}", environment.display_string());
 }
 
 #[cfg(test)]
@@ -56,6 +156,11 @@ mod tests {
     use super::*;
 
     const SAMPLE_INPUT: &'static [u8] = include_bytes!("../data/sample");
+
+    #[test]
+    fn test_environment_generation() {
+        let _environment = parse_environment(SAMPLE_INPUT);
+    }
 
     #[test]
     fn test_line_parsing() {
